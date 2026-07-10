bbd9d073f0468187125085be9c7db832ec8620f7
harshG775 <hgaur491@gmail.com>
2026-07-10 18:51:11 +0530

    feat(db): add relations to database drizzle configuration

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,8 @@
 import { neon } from "@neondatabase/serverless"
 import { drizzle } from "drizzle-orm/neon-http"
 import { env } from "#/env"
+import { relations } from "./relations"
 
 const sql = neon(env.DATABASE_URL)
 
-export const db = drizzle({ client: sql })
+export const db = drizzle({ client: sql, relations: relations })
