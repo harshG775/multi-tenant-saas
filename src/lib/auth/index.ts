eddcade9e654e6fc9ad3d0609f64185a5c9b5658
Harsh_G <hgaur491@gmail.com>
2026-09-19 13:50:27 +0530

    setup: drizzle and better-auth

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { env } from "#/env";
+import { db } from "#/lib/db";
+import * as schema from "#/lib/db/schema/index";
+
+export const auth = betterAuth({
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.SERVER_URL,
+    basePath: "/api/v1/auth",
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema: schema,
+    }),
+    plugins: [tanstackStartCookies()],
+});
